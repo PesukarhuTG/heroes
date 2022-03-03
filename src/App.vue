72d54bd2f39392a061a6f4cd9faf6ee3d40b43e4
Tatiana <ganzhinovat@yandex.ru>
2022-03-03 13:54:38 +0300

    feat: add Vue components

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+    <div id="app">
+
+        <app-header/>
+
+        <div class="container">
+            <h1 class="pt-3 pb-3">Персонажи Marvel</h1>
+
+            <app-modal/>
+
+            <spinner/>
+
+            <div class="row">
+                <div class="card mb-3" style="max-width: 540px;">
+                  <div class="row g-0">
+                    <div class="col-md-4">
+                      <img src="https://vgtimes.ru/uploads/posts/2021-10/82020_1_1.jpg" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                      <div class="card-body">
+                        <h5 class="card-title">Человек-паук</h5>
+                        <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                          Подробнее
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    import Spinner from "./components/Spinner";
+    import AppModal from "./components/AppModal";
+    import AppHeader from "./components/AppHeader";
+
+    export default {
+        name: 'App',
+        components: {
+            AppHeader,
+            AppModal,
+            Spinner,
+        },
+        data() {
+            return {
+                loading: false,
+                characters: [],
+                characterIndex: 0,
+            }
+        },
+        methods: {},
+        computed: {},
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/AppModal.vue
@@ -0,0 +1,40 @@
+<template>
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Человек-паук</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <img src="https://vgtimes.ru/uploads/posts/2021-10/82020_1_1.jpg" class="img-fluid rounded-start" alt="poster">
+            <div>
+              <h5>Описание</h5>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto a tempore nobis possimus rerum
+                molestias, natus expedita quibusdam praesentium. Incidunt ullam reprehenderit in ut dolorem vitae illum
+                porro, laboriosam exercitationem.</p>
+              <h5>Комиксы</h5>
+              <ul>
+                <li>Adventures of Spider-Man (1996) #1</li>
+                <li>Adventures of Spider-Man (1996) #2</li>
+                <li>Adventures of Spider-Man (1996) #3</li>
+              </ul>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AppModal",
+    }
+</script>
+
+<style scoped>
+
+</style>
