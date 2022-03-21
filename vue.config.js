c00ec3d950dd5f9982fadce4888f90e54486aa25
Tatiana <ganzhinovat@yandex.ru>
2022-03-21 14:26:53 +0300

    refactor: delete last changes

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/heroes/'
-    : '/'
-}
-
-module.exports = {
   pwa: {
     name: 'Marvel',
     themeColor: '#212121'
